81702472148e89536d6af07454b1c802a971024b
Charles <charles.chen@yjtech.tw>
2021-01-14 17:13:14 +0800

    修正子類別建構子未新增相對應參數的問題

--- ConsoleApp12112020/BNB.cs
+++ ConsoleApp12112020/BNB.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp12112020
 {
     class BNB : Hotel
     {
-       
-        
+        public BNB() : this(1, 888, "Taichung", 1)
+        {
+            Console.WriteLine("Initialization by default construct");
+        }
+        public BNB(int guestNum1, int roomNum1, string location1, int roomLeft1) : base(guestNum1, roomNum1, location1, roomLeft1)
+        {
+            Console.WriteLine("Initialization by parameter construct");
+        }
     }
 }
