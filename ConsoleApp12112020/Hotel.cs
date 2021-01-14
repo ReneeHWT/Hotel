d13525c627edab504f5336d5ef87ba773fb6ba6a
ReneeHWT <73097435+ReneeHWT@users.noreply.github.com>
2021-01-14 15:39:17 +0800

    Add files via upload

---
+++ ConsoleApp12112020/Hotel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12112020
+{
+    public class Hotel
+    {
+        public int guestNum;
+        public int roomNum;
+        public string location;
+        public int roomLeft;
+
+
+        public Hotel(int guestNum1, int roomNum1, string location1, int roomLeft1)
+        {
+            guestNum = guestNum1;
+            roomNum = roomNum1;
+            location = location1;
+            roomLeft = roomLeft1;
+        }
+
+        public void MakeReservation()
+        {
+            Console.WriteLine("Make a reservation now.");
+        }
+
+        public bool Avail()
+        {
+            if (roomLeft < 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
+    
+
+   
+}
---
+++ ConsoleApp12112020/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace ConsoleApp12112020
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Hotel marriot = new Hotel(2, 1, "Taipei",0);
+            Hotel hilton = new Hotel(4, 2, "Kaohsiung",8);
+            BNB howard = new BNB(3,2, "Taoyuan", 0);//BNB可以繼承Hotel 的建構式嗎？
+
+            
+
+            Console.WriteLine(marriot.guestNum +" guests need " +marriot.roomNum + " room(s) in "+ marriot.location +".");
+            Console.WriteLine("Is Marriot available?");
+            Console.WriteLine(marriot.Avail());
+            Console.ReadLine();
+
+
+            Console.WriteLine(hilton.guestNum + " guests need " + hilton.roomNum + " room(s) in " + hilton.location + ".");
+            Console.WriteLine("Is Hilton available?");
+            Console.WriteLine(hilton.Avail());
+            Console.ReadLine();
+
+            
+           
+            Console.WriteLine(howard.guestNum + " guests need " + howard.roomNum + " room(s) in " + howard.location + ".");
+            Console.WriteLine("Is Howard available?");
+            Console.WriteLine(howard.Avail());
+            Console.ReadLine();
+        }
+            
+          
+
+    }
+
+    
+}
